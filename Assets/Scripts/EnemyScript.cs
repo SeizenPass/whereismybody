311676de2f999b056f1ed1e780646af8cfc05c26
Amiran Kurman <amiranfox@gmail.com>
2021-01-29 11:11:33 +0600

    fixed collider bug

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -8,7 +8,6 @@ public class EnemyScript : MonoBehaviour
     GroundDetector groundDetector;
 
     private GameObject player;
-
     private PossessScript ps;
     // Start is called before the first frame update
     void Start()
